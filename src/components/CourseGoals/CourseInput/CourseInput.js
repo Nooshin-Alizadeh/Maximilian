d39f30304e45d1559eb2b1bbf9686de7ce6c7338
Nooshin-Alizadeh <nooshin.alizadeh.behjati@gmail.com>
2023-02-10 14:39:41 +0330

    Styled Components & Dynamic Props Styled Components & Media Queries

--- src/components/CourseGoals/CourseInput/CourseInput.js
+++ src/components/CourseGoals/CourseInput/CourseInput.js
@@ -1,8 +1,46 @@
 import React, { useState } from "react";
 
+import styled from "styled-components";
 import Button from "../../UI/Button/Button";
 import "./CourseInput.css";
 
+const FormControl = styled.div`
+  margin: 0.5rem 0;
+
+  & label {
+    font-weight: bold;
+    display: block;
+    margin-bottom: 0.5rem;
+    color: ${(props) => (props.invalid ? "red" : "pink")};
+  }
+
+  & input {
+    display: block;
+    width: 100%;
+    border: 1px solid
+      ${(props) => {
+        return props.invalid ? "red" : "#ccc";
+      }};
+    backgrounf: ${(props) => (props.invalid ? "#ffd7d7" : "transparent")};
+    font: inherit;
+    line-height: 1.5rem;
+    padding: 0 0.25rem;
+  }
+
+  & input:focus {
+    outline: none;
+    background: #fad0ec;
+    border-color: #8b005d;
+  }
+`;
+/**&.invalid input {
+  border-color: red;
+  background: #ffd7d7;
+}
+&.invalid label {
+  color: red;
+  text-shadow: #ffd7d7;
+} */
 const CourseInput = (props) => {
   const [enteredValue, setEnteredValue] = useState("");
   const [isValid, setIsValid] = useState(true);
@@ -26,7 +64,8 @@ const CourseInput = (props) => {
 
   return (
     <form onSubmit={formSubmitHandler}>
-      <div className={`form-control ${isValid ? "" : "invalid"}`}>
+      {/* <FormControl className={!isValid && 'invalid' }> */}
+      <FormControl invalid={!isValid}>
         <label style={{ color: isValid ? "black" : "red" }}>Course Goal</label>
         <input
           type="text"
@@ -36,7 +75,7 @@ const CourseInput = (props) => {
             background: isValid ? "transparent" : "salmon",
           }}
         />
-      </div>
+      </FormControl>
       <Button type="submit">Add Goal</Button>
     </form>
   );
